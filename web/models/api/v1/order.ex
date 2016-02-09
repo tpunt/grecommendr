8dbed62e4994e68cde29f4b5657651216cd61679
Thomas Punt <tpunt@hotmail.co.uk>
2016-02-09 18:43:22 +0000

    Refactor Order model

--- web/models/api/v1/order.ex
+++ web/models/api/v1/order.ex
@@ -7,49 +7,34 @@ defmodule GameRecommender.Api.V1.Order do
         %{"user_id" => user_id} ->
           """
             MATCH (User {userId: #{user_id}})-[:PURCHASED]->(order:Order)-[:CONTAINS]->(game:Game)
-            RETURN order, game.gameId AS gameId
+            RETURN order, collect(game.gameId) AS games
           """
         _ ->
           """
             MATCH (order:Order)-[:CONTAINS]->(game:Game)
-            RETURN order, game.gameId AS gameId
+            RETURN order, collect(game.gameId) AS games
           """
       end
 
-    Neo4j.query!(Neo4j.conn, cypher_query)
-    |> compact_orders
+    case Neo4j.query!(Neo4j.conn, cypher_query) do
+      [] ->
+        raise "No orders found"
+      order ->
+        order
+    end
   end
 
   def fetch_order(order_id) do
     cypher_query = """
       MATCH (order:Order {orderId: #{order_id}})-[:CONTAINS]->(game:Game)
-      RETURN order, game.gameId AS gameId
+      RETURN order, collect(game.gameId) AS games
     """
 
-    order =
-      Neo4j.query!(Neo4j.conn, cypher_query)
-      |> compact_orders
-
-    case order do
+    case Neo4j.query!(Neo4j.conn, cypher_query) do
       [] ->
         raise "Order not found"
       [order] ->
         order
     end
-  end
-
-  defp compact_orders(orders) do
-    orders
-    |> Enum.reduce([], fn (data = %{"gameId" => gid, "order" => %{"orderId" => oid}}, new_data) ->
-        game = [gid]
-        new_data =
-          if idx = Enum.find_index(new_data, fn (new_data) -> new_data["order"]["orderId"] === oid end) do
-            data = Enum.at(new_data, idx)
-            games = Enum.into(data["games"], game)
-            List.replace_at(new_data, idx, %{"games" => games, "order" => data["order"]})
-          else
-            Enum.concat(new_data, [%{"games" => game, "order" => data["order"]}])
-          end
-      end)
   end
 end
