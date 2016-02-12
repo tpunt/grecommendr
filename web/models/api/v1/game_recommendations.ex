17c3e19ea87d11cebd796f679d8d597f1176929a
Thomas Punt <tpunt@hotmail.co.uk>
2016-02-12 09:42:23 +0000

    Continue work on recommendations

--- web/models/api/v1/game_recommendations.ex
+++ web/models/api/v1/game_recommendations.ex
@@ -54,112 +54,204 @@ defmodule GameRecommender.Api.V1.GameRecommendations do
     # use her algorithm to calculate similarity of neighbourhood
 
     cypher_query = """
-    MATCH (user1:User {userId: #{user_id}})-[user1_game1_rating:RATES]->(game1:Game)<-[user2_game1_rating:RATES]-(user2:User)-[user2_game2_rating:RATES]->(game2:Game)
-    RETURN user1, game1, user1_game1_rating, user2, user2_game1_rating, game2, user2_game2_rating
+    MATCH (user:User {userId: #{user_id}})-[game_rating:RATES]->(game:Game)
+    RETURN user, game_rating, game
     """
 
     cypher_query2 = """
-    MATCH (User {userId: #{user_id}})-[:RATES]->(game:Game)
-    RETURN collect(game.gameId) AS games
+    MATCH (User {userId: #{user_id}})-[:RATES]->(Game)<-[:RATES]-(u:User)
+    WITH collect(u.userId) AS ids
+    MATCH (user:User)-[game_rating:RATES]->(game:Game)
+    WHERE user.userId IN ids
+    RETURN user, game, game_rating
     """
 
-    user_rated_games = Neo4j.query!(Neo4j.conn, cypher_query2)
+    [user_a] =
+      Neo4j.query!(Neo4j.conn, cypher_query)
+      |> restructure_user_game_rating_data
 
-    data = Neo4j.query!(Neo4j.conn, cypher_query)
+    owned_games_ids =
+      get_user_games(user_id)
+      |> Enum.map(fn (%{"game" => %{"gameId" => game_id}}) -> game_id end)
 
     data =
-      data
+      Neo4j.query!(Neo4j.conn, cypher_query2)
       # reduce the pulled data into an easier to work with structure
-      |> Enum.reduce([], fn (data, new_data) ->
-          user1_game1 = [%{"game" => data["game1"], "rating" => data["user1_game1_rating"]["rating"]}]
-          user2_game1 = [%{"game" => data["game1"], "rating" => data["user2_game1_rating"]["rating"]}]
-          user2_game2 = [%{"game" => data["game2"], "rating" => data["user2_game2_rating"]["rating"]}]
-          new_data =
-            if idx = Enum.find_index(new_data, fn (new_data) -> new_data.user["userId"] === data["user1"]["userId"] end) do
-              # the user already exists, so update it
-              data2 = Enum.at(new_data, idx)
+      |> restructure_user_game_rating_data(user_a)
+      # set the neighbourhood threshold of requiring at least 3 overlapping game ratings
+      |> Enum.filter(fn (data) ->
+          data.game_overlap > 0 # update to 2 later
+        end)
+      # remove any users who have rated the exact games only
+      |> Enum.filter(fn (data) ->
+          data.game_overlap !== Enum.count(data.games)
+        end)
+      # remove any users who have only rated games user A already owns
+      |> Enum.filter(fn (data) ->
+          # update this line to use the convenient game_ids property
+          Enum.any?(data.games, fn (%{"game" => %{"gameId" => game_id}}) -> !Enum.member?(owned_games_ids, game_id) end)
+        end)
 
-              if Enum.find_index(data2.games, fn (game) -> game["game"]["gameId"] === data["game1"]["gameId"] end) do
-                # the game already exists, so do nothing
-                new_data
+    diff_game_ids =
+      data
+      # Get lists of the overlapping games that are not on user A's rating list
+      |> Enum.map(fn (data) ->
+          data.games
+          |> Enum.map(fn (%{"game" => %{"gameId" => game_id}}) ->
+              unless Enum.any?(user_a.games, fn (%{"game" => %{"gameId" => r_game_id}}) -> r_game_id === game_id end), do: game_id
+            end)
+          |> Enum.filter(fn (game_ids) -> game_ids !== :nil end)
+        end)
+      # flatten the list
+      |> List.flatten
+      # count the number of occurrences for each game ID
+      |> Enum.reduce([], fn (game_id, new_data) ->
+          if idx = Enum.find_index(new_data, fn (data) -> data.game_id === game_id end) do
+            data = Enum.at(new_data, idx)
+            List.replace_at(new_data, idx, update_in(data.count, &(&1 + 1)))
+          else
+            [%{game_id: game_id, count: 1} | new_data]
+          end
+        end)
+      # order the game ids list by the most commonly occurring
+      |> Enum.sort(fn (%{game_id: id_x, count: c_x}, %{game_id: id_y, count: c_y}) ->
+          c_x > c_y
+        end)
+      # just retrieve the game IDs now
+      |> Enum.map(fn (%{game_id: id}) -> id end)
+
+# user_a.games
+# data.games
+# IO.inspect diff_game_ids
+
+    diff_game_ids
+    |> Enum.map(fn (game_id) ->
+        table = %{predict_for: game_id, games: []}
+
+        Enum.reduce(user_a.games, table, fn (%{"game" => %{"gameId" => gid1}, "rating" => rating1}, table) ->
+          Enum.reduce(data.games, table, fn (%{"game" => %{"gameId" => gid2}, "rating" => rating2}, table) ->
+            if gid1 === gid2 do
+              if idx = Enum.find_index(table.games, fn (%{"game" => %{"gameId" => gid3}}) -> gid2 === gid3 end) do
+                game = Enum.at(table.games, idx)
+                List.replace_at(table.games,
+                  idx,
+                  %{game: game, })
+                # if the game exists, update it
               else
-                games = Enum.into(data2.games, user1_game1)
-                #why game overlap here?
-                List.replace_at(new_data, idx, %{user: data2.user, games: games, game_overlap: data2.game_overlap + 1})
+                #insert it
               end
             else
-              #why game overlap here?
-              Enum.concat(new_data, [%{user: data["user1"], games: user1_game1, game_overlap: 1}])
+              table
             end
+          end)
+        end)
+      end)
 
-          new_data =
-            if idx = Enum.find_index(new_data, fn (new_data) -> new_data.user["userId"] === data["user2"]["userId"] end) do
-              # the user already exists, so update it
-              data2 = Enum.at(new_data, idx)
+    diff_game_ids
+    |> Enum.map(fn (game_id) ->
+        neighbour_similarities = Enum.reduce(data, [], fn (data2, new_data) ->
+          if Enum.member?(data2.game_ids, game_id) do
+            Enum.map(user_a.games, fn (%{"game" => %{"gameId" => gid1}, "rating" => rating1}) ->
+              if idx = Enum.find_index(data2.games, fn (%{"game" => %{"gameId" => gid2}}) -> gid2 === gid1 end) do
+                game = Enum.at(data2.games, idx)
+                %{"game" => %{"gameId" => gid}, "rating" => rating2} = game
 
-              if Enum.find_index(data2.games, fn (game) -> game["game"]["gameId"] === data["game1"]["gameId"] end) do
-                # the game already exists, so do nothing
-                new_data
-              else
-                games = Enum.into(data2.games, user2_game1)
-                List.replace_at(new_data, idx, %{user: data2.user, games: games, game_overlap: data2.game_overlap + 1})
+
+
+                # user_a = user_id
+                # user_b = data2.user["user_id"]
+                # game_x = gid
+                # user_a_rating = rating1
+                # user_b_rating = rating2
+                # user_a_avg_rating = user_a.avg_game_rating
+                # user_b_avg_rating = data2.avg_game_rating
+
+                #
               end
-            else
-              game_overlap = 1
-# IO.puts "game 2 (ever) in user1 ratings?"
-# IO.inspect user_rated_games
-# IO.inspect data["game2"]["gameId"]
-              [%{"games" => rated_games}] = user_rated_games
-
-              if Enum.member?(rated_games, data["game2"]["gameId"]) do
-                game_overlap = 2
-              end
-
-              Enum.concat(new_data, [%{user: data["user2"], games: Enum.concat(user2_game1, user2_game2), game_overlap: game_overlap}])
-            end
+            end)
+          else
+            0
+          end
         end)
-      # set the threshold of requiring at least 3 overlapping game ratings
-      |> Enum.filter(fn (data) ->
-          data.game_overlap > 1 # update to 2 later
-        end)
-      # remove any users who have rated the exact games only (except for the user at hand)
-      |> Enum.filter(fn (data) ->
-          # IO.inspect data.user["userId"]
-          # IO.inspect user_id
-          # IO.inspect "#{data.user["userId"]}" === user_id
-          !(data.game_overlap === Enum.count(data.games) && "#{data.user["userId"]}" !== user_id)
-        end)
-      # rank the neighbourhood in terms of items they have most in common (that are not on the user at hand's rating list)
-      # |> ...
+      end)
 
-    IO.inspect data
+    # IO.inspect data
+    IO.inspect ExMatrix.new_matrix(2,2)
+  end
 
-    Enum.at(data, 0).games
+  defp sim(user_a, user_b) do
+    sum1 = user_a_rating - user_a_avg_rating
+    sum2 = user_b_rating - user_b_avg_rating
 
-    # negative_games = get_user_disliked_games user_id
-    # neutral_games = get_user_neutral_games user_id
-    # positive_games = get_user_liked_games user_id
-    #
-    # rated_games =
-    #   negative_games
-    #   |> Enum.concat neutral_games
-    #   |> Enum.concat positive_games
-    #
-    # games_owned = get_user_games user_id
-    #
-    # all_games
-    # # remove all games the user has already purchased
-    # |> Enum.filter(fn (%{"game" => %{"gameId" => game_id}}) ->
-    #     !Enum.any?(games_owned, fn (%{"game" => %{"gameId" => game_id_2}}) ->
-    #       game_id === game_id_2
-    #     end)
-    #   end)
-    # # remove all games the user has rated (since they already know of those games)
-    # |> Enum.filter(fn (%{"game" => %{"gameId" => game_id}}) ->
-    #     !Enum.any?(rated_games, fn (%{"games" => %{"gameId" => game_id_2}}) ->
-    #       game_id === game_id_2
-    #     end)
-    #   end)
+    (sum sum1 * sum2)
+    /
+    (:math.sqrt(sum1 * sum1) * :math.sqrt(sum2 * sum2))
+  end
+
+  defp pred(user_a, user_b, item_a, n) do
+    (sum y - sim(user_a, user_b))
+    /
+    n
+  end
+
+  defp restructure_user_game_rating_data(data, user_a) do
+    data
+    |> Enum.reduce([], fn (data, new_data) ->
+        game = %{"game" => data["game"], "rating" => data["game_rating"]["rating"]}
+        game_id = data["game"]["gameId"]
+
+        if idx = Enum.find_index(new_data, fn (new_data) -> new_data.user["userId"] === data["user"]["userId"] end) do
+          data2 = Enum.at(new_data, idx)
+          games = Enum.into(data2.games, [game])
+          game_overlap = if Enum.any?(user_a.games, fn (%{"game" => %{"gameId" => r_game_id}}) -> r_game_id === game_id end), do: 1, else: 0
+
+          List.replace_at(new_data,
+            idx,
+            %{user: data2.user,
+              games: games,
+              game_ids: [game_id | data2.game_ids],
+              game_overlap: data2.game_overlap + game_overlap,
+              avg_game_rating: (data2.avg_game_rating * data2.game_rating_count + game["rating"]) / (data2.game_rating_count + 1),
+              game_rating_count: data2.game_rating_count + 1})
+        else
+          game_overlap = if Enum.any?(user_a.games, fn (%{"game" => %{"gameId" => r_game_id}}) -> r_game_id === game_id end), do: 1, else: 0
+          Enum.concat(new_data,
+            [%{user: data["user"],
+              games: [game],
+              game_ids: [game_id],
+              game_overlap: game_overlap,
+              avg_game_rating: game["rating"],
+              game_rating_count: 1}])
+        end
+      end)
+  end
+
+  defp restructure_user_game_rating_data(data) do
+    data
+    |> Enum.reduce([], fn (data, new_data) ->
+        game = %{"game" => data["game"], "rating" => data["game_rating"]["rating"]}
+        game_id = data["game"]["gameId"]
+
+        if idx = Enum.find_index(new_data, fn (new_data) -> new_data.user["userId"] === data["user"]["userId"] end) do
+          data2 = Enum.at(new_data, idx)
+          games = Enum.into(data2.games, [game])
+
+          List.replace_at(new_data,
+            idx,
+            %{user: data2.user,
+              games: games,
+              game_ids: [game_id | data2.game_ids],
+              avg_game_rating: (data2.avg_game_rating * data2.game_rating_count + game["rating"]) / (data2.game_rating_count + 1),
+              game_rating_count: data2.game_rating_count + 1})
+        else
+          Enum.concat(new_data,
+            [%{user: data["user"],
+              games: [game],
+              game_ids: [game_id],
+              avg_game_rating: game["rating"],
+              game_rating_count: 1}])
+        end
+      end)
   end
 
   defp recommend(%{"user_id" => user_id, "type" => "top_rated"}) do
@@ -219,11 +311,6 @@ defmodule GameRecommender.Api.V1.GameRecommendations do
       |> Enum.concat(get_user_liked_games user_id)
       |> Enum.uniq_by(fn(%{"games" => %{"gameId" => game_id}}) -> game_id end)
 
-    # positive_genres =
-    #   positive_games
-    #   |> Enum.map(fn (%{"games" => %{"genre" => genre}}) -> genre end)
-    #   |> Enum.uniq_by(&(&1))
-
     positive_tags =
       positive_games
       |> Enum.map(fn (%{"games" => %{"tags" => tags}}) -> tags end)
@@ -231,17 +318,6 @@ defmodule GameRecommender.Api.V1.GameRecommendations do
       |> Enum.uniq_by(&(&1))
 
     negative_games = get_user_disliked_games user_id
-    #
-    # negative_genres =
-    #   negative_games
-    #   |> Enum.map(fn (%{"games" => %{"genre" => genre}}) -> genre end)
-    #   |> Enum.uniq_by(&(&1))
-    #
-    # negative_tags =
-    #   negative_games
-    #   |> Enum.map(fn (%{"games" => %{"tags" => tags}}) -> tags end)
-    #   |> List.flatten
-    #   |> Enum.uniq_by(&(&1))
 
     games_owned = get_user_games user_id
 
@@ -267,7 +343,7 @@ defmodule GameRecommender.Api.V1.GameRecommendations do
         end)
       # calculate what weighting each game will have from tag similarity an purchase count
       |> Enum.map(fn (game = %{tag_similarity: tag_similarity, game: %{"game" => %{"purchaseCount" => purchase_count}}}) ->
-          %{weighting: purchase_count * 0.7 + tag_similarity * 0.3,
+          %{weighting: (2 * purchase_count * tag_similarity) / (purchase_count + tag_similarity),
           game: game.game}
         end)
       # sort the games according to their calculated weighting
@@ -276,8 +352,6 @@ defmodule GameRecommender.Api.V1.GameRecommendations do
         end)
       # remove game weighting - this does not need to be exposed via the web service
       |> Enum.map(fn (game) -> game.game end)
-
-    # weighting: (2 * download_count * tag_similarity) / (download_count + tag_similarity)
 
     # sorted =
     #   tailored_games
